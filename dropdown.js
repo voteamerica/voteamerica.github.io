a36a05d6740915c1b0658b30cd6d55f328f89cfc
dbrusky1 <dmbrusky@gmail.com>
2016-09-24 16:24:51 -0400

    working

--- dropdown.js
+++ dropdown.js
@@ -1,9 +1,26 @@
+// dropdown.js  
+// Loads JSON data to dropdown template 
+
 var votingDropdown = (function () {
 	// grab data from csv
 	// loop through and print statename in dropdown with hyperlink
-	console.log('test');
-	jQuery.get('res/state_election_details.csv', function(data){
-		console.log(data)
+	$.getJSON( "res/voting-details.json", function( data ) {
+  		var items = [];
+  		$.each( data, function( key, val ) {
+    		items.push( "<option class='state-option' id='" + key + "' value='" + val["Polling location finder"] + "' >" + val["Voter registration check"] + "</option>" );
+  		});
+  		$( "<select />", {
+   			 "class": "state-dropdown",
+   			 "id": "state-select",
+    			html: items.join( "" )
+  		}).appendTo( "#state-details" );
+
+
+  		$("#state-select").change(function() {
+        		console.log("change"); 
+        		window.location.href = this.value;  
+    	
+    	});
 	});
 
 
