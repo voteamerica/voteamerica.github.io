7f70cb2d322620c700fe0dfefb4aea550ce79b82
dbrusky1 <dmbrusky@gmail.com>
2016-09-24 12:58:59 -0400

    adding json and js file

---
+++ dropdown.js
@@ -0,0 +1,11 @@
+var votingDropdown = (function () {
+	// grab data from csv
+	// loop through and print statename in dropdown with hyperlink
+	console.log('test');
+	jQuery.get('res/state_election_details.csv', function(data){
+		console.log(data)
+	});
+
+
+
+}());
