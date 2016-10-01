3b8b340dc50f6c4431f8f2954d9dee3eb94552f2
dbrusky1 <dmbrusky@gmail.com>
2016-10-01 12:07:03 -0400

    new branch to solve merge conflicts

--- dropdown.js
+++ dropdown.js
@@ -2,43 +2,57 @@
 // Loads JSON data to dropdown template 
 
 var votingDropdown = (function() {
+
 	// grab data from csv
 	// loop through and print statename in dropdown with hyperlink
 	$.getJSON("res/voting-details.json", function(data) {
 		var items = [];
-		
+
 		$.each(data, function(key, val) {
-			items.push("<option class='state-option' id='" + key + "' value='" + val["Voter registration check"] + "' >" + val["State"] + "</option>");
+			items.push("<li class='state-location'>" + "<a href='" + val["Voter registration check"] + "' target='_blank'  id='" + key + "' >" + val["State"] + "</a>" + "</li>");
 		});
-		
-		$("<select />", {
+
+		$("<ul />", {
 			"class": "state-dropdown",
 			"id": "state-select",
 			html: items.join("")
 		}).appendTo("#state-details");
 
-		$("#state-select").change(function() {
-			window.open(this.value);
+		$("#showState-a").click(function() {
+			$(".overlay-a").fadeIn(500);
 		});
 
-		
+		$("#modal-a").on("click", function() {
+			$(".overlay-a").fadeOut(500);
+
+		});
+
 	});
 
 	$.getJSON("res/voting-details.json", function(data) {
+
 		var itemsb = [];
+
 		$.each(data, function(key, val) {
-			itemsb.push("<option class='state-option' id='" + key + "' value='" + val["Polling location finder"] + "' >" + val["State"] + "</option>");
+			itemsb.push("<li class='state-location'>" + "<a href='" + val["Polling location finder"] + "' target='_blank'  id='" + key + "' >" + val["State"] + "</a>" + "</li>");
 		});
 
-		$("<select />", {
+		$("<ul />", {
 			"class": "location-dropdown",
-			"id": "location-select",
+			"id": "state-select",
 			html: itemsb.join("")
 		}).appendTo("#location-details");
 
-		$("#location-select").change(function() {
-			window.open(this.value);
-		});
-    });
+	});
+
+	$("#showStates-b").click(function() {
+		$(".overlay-b").fadeIn(500);
+
+	});
+
+	$("#modal-b").on("click", function() {
+		$(".overlay-b").fadeOut(500);
+
+	});
 
 }());
