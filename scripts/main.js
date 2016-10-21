2b26563a4b1777abf5e9421655cdb643e45b6900
Richard Westenra <rjwestenra@gmail.com>
2016-10-21 13:13:49 +0100

    Begin work on Available Times List UI version 2
    - Remove jQuery datetimepicker, range slider, moment.js and dateformat.js
    - Build a new, more accessible interface using native HTML5 date/time inputs

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 $(function(){
     var $forms = $('#forms'),
         $intro = $('#intro'),
+        rowTemplate = $('#available-time-row').html(),
         supportsHistoryApi =  !!(window.history && history.pushState);
+
+    toggleFormsOnHashChange();
+    window.onpopstate = toggleFormsOnHashChange;
 
     function scrollTo(offset, speed) {
         $('html, body').animate({
@@ -57,195 +61,93 @@ $(function(){
         e.preventDefault();
     });
 
-    $forms.find('.available-times-list')
-        .on('click', '.remove-time', function() {
-            var $timesList = $(this).parents('.available-times-list');
-            $(this).parent().remove();
-            updateHiddenJSONTimes($timesList);
-        });
-
     $forms.on('change', '.toggleRequiredEmail', function(){
         var id = $(this).attr('data-emailID');
         $forms.find(id).prop('required', $(this).is(':checked')).trigger('change');
     });
 
     $forms.on('submit', 'form', function() {
-        updateHiddenJSONTimes( $(this).find('.available-times-list') );
+        updateHiddenJSONTimes( $(this).find('.available-times') );
     });
 
-    toggleFormsOnHashChange();
-    window.onpopstate = toggleFormsOnHashChange;
+    $forms.find('.available-times').each(function() {
+        var $self = $(this),
+            type = $self.attr('data-type');
 
-    $('.redirect').val(window.location.origin + window.location.pathname + 'thanks.html');
-
-    var currentDateString = $.format.date(new Date(), "ddd, MMM d");
-    $('#driver-button-date, #rider-button-date').text(currentDateString);
-    
-
-    function initDatePicker($datetime, $datepicker) {
-        // Initialize datepickers
-        // Max Date = Voting Day 2016!'
-        $datepicker.datetimepicker({
-            timepicker: false,
-            format: 'l, M j',
-            startDate: '2016/10/05',
-            minDate: '2016/10/05',
-            maxDate: '2016/11/08',
-            yearStart: 2016,
-            yearEnd: 2016,
-            inline: true
-        });
-
-        // Must manually set the initial value of the calendar input
-        // Needs to be today's date.
-        var currDate = new Date();
-        $datepicker.attr('value', currDate);
-
-        $datepicker.on('change', function() {
-            $datetime.find('.button-date').text($(this).val());
-        });
-    }
-
-    function initTimeSlider($datetime, slider) {
-        noUiSlider.create(slider, {
-            start: [360, 1440],
-            connect: true,
-            step: 10,
-            range: {
-                'min': [360],
-                'max': [1320]
-            },
-            pips: {
-                mode: 'values',
-                density: 2,
-                values: [480, 720, 960, 1200, ],
-                format: {
-                    to: formatTime
-                }
+        function addRow(i, hideDeleteButton) {
+            var $row = $(rowTemplate.replace(/{{type}}/g, type).replace(/{{i}}/g, i));
+            $row.find('.form-input--date').attr('min', yyyymmdd());
+            if (hideDeleteButton) {
+                $row.find('.remove-time').hide();
             }
-        });
+            $self.append($row);
+        }
 
-        slider.noUiSlider.on('update', function() {
-            var sliderData = slider.noUiSlider.get();
-            var startTime = formatTime(sliderData[0]);
-            var endTime = formatTime(sliderData[1]);
-            $datetime.find('.add-time-btn')
-                .children('.button-time')
-                .text(startTime + '-' + endTime);
-        });
-    }
+        function toggleRemoveTimeBtn(rowCount) {
+            $self.find('.remove-time').toggle(rowCount > 1);
+        }
 
-    function addTimeBtnListener($datetime, $slider, $timesList, $datepicker) {
-        $datetime.on('click', '.add-time-btn', function(e) {
-            var sliderData = $slider[0].noUiSlider.get();
-            var startTime = formatTime(sliderData[0]);
-            var endTime = formatTime(sliderData[1]);
+        addRow(0, true);
 
-            var date = $datepicker.val();
-            var niceDate = $.format.date(date, "ddd D MMMM");
-
-            // not comfortable with this, but can't see why don't get this from datepicker
-            var datePlusYear = date + " 2016";
-
-            var niceDateYear = $.format.date(datePlusYear, "ddd D MMMM yyyy");
-            var startDateTime = niceDateYear + ' ' + startTime;
-            var endDateTime = niceDateYear + ' ' + endTime;
-
-            var ndt = $.format.date(datePlusYear, "dd MMMM yyyy");
-            var sdt = moment(ndt);
-            var edt = moment(ndt);
-
-            var sliderStartTime = formatTime2(sliderData[0]);
-            var sliderEndTime   = formatTime2(sliderData[1]);
-
-            var isoStart        = getISOString(sdt, sliderStartTime);
-            var isoEnd          = getISOString(edt, sliderEndTime);
-
-
-            var cancelButton = ' <button class="remove-time button--cancel" aria-label="Delete time">&times;</button>';
-
-            var $listItem = $('<li/>')
-                .data('datetime', [isoStart, isoEnd].join('/'))
-                .html(niceDate + ', ' + startTime + '—' + endTime + cancelButton);
-
-            $timesList.append($listItem);
-
-            updateHiddenJSONTimes($timesList);
-
+        $self.siblings('.add-time-btn').on('click', function(e) {
+            var $rows = $self.find('li'),
+                rowCount = $rows.length;
+            addRow(rowCount);
+            toggleRemoveTimeBtn(rowCount + 1);
+            updateHiddenJSONTimes($self);
+            $self.parents('form').validator('update');
             e.preventDefault();
+        });
+
+        $self.on('click', '.remove-time', function(e) {
+            $(this).parent().remove();
+            toggleRemoveTimeBtn($self.find('li').length);
+            updateHiddenJSONTimes($self);
+            $self.parents('form').validator('update');
+            e.preventDefault();
+        });
+    });
+
+    function getDateTimeValues($timesList) {
+        return $timesList.find('li').get().map(function(li) {
+            return formatTime(
+                $(li).find('.form-input--date').val(),
+                $(li).find('.form-input--time-start').val(),
+                $(li).find('.form-input--time-end').val()
+            );
         });
     }
 
     function updateHiddenJSONTimes($timesList) {
-        var timeData = $timesList.find('li').get().map(function(li) {
-            return $(li).data('datetime');
-        }).join('|');
-
-        $timesList.siblings('.hiddenJSONTimes')
-            .val(timeData)
-            .trigger('change');
+        var timeData = getDateTimeValues($timesList);
+        $timesList.siblings('.hiddenJSONTimes').val(timeData.join('|'));
     }
 
-    $('.date-time-pickers').each(function() {
-        // Add selected date and time to list of available times on button click
-        var $slider = $(this).find('.time-range-slider'),
-            $timesList = $(this).find('.available-times-list'),
-            $datepicker = $(this).find('.datepicker');
-
-        initDatePicker($(this), $datepicker);
-
-        initTimeSlider($(this), $slider.get(0));
-
-        addTimeBtnListener($(this), $slider, $timesList, $datepicker);
-    });
-
-    function getISOString(date, timeAdjustment) {
-        var isoString = '';
-
-        date = date.add(timeAdjustment.mins, 'm');
-        date = date.add(timeAdjustment.hour, 'h');
-
-        if (timeAdjustment.am === false && timeAdjustment.hour !== 12) {
-            date = date.add(12, 'h');
-        }
-
-        isoString = date.toISOString();
-
-        return isoString;
-    }
-
-    // The sliders work with the day in terms of minutes, this function translates the minutes
-    // into readable format i.e. 4:34pm
-    function formatTime(minutes) {
-        var am = false;
-        if (minutes < 720) {
-            am = true;
-        }
-        var hour = Math.floor(minutes / 60);
-        hour = minutes < 780 ? hour : hour - 12;
-        var out_minutes = minutes % 60;
-        out_minutes = out_minutes < 10 ? '0' + out_minutes : out_minutes.toString();
-        var ampmString = am ? 'am' : 'pm';
-
-        return hour + ':' + out_minutes + ampmString;
-    }
-
-    function formatTime2(minutes) {
-        var am = false;
-        if (minutes < 720) {
-            am = true;
-        }
-        var hour = Math.floor(minutes / 60);
-        hour = minutes < 780 ? hour : hour - 12;
-        var out_minutes = minutes % 60;
-        out_minutes = out_minutes < 10 ? '0' + out_minutes : out_minutes.toString();
-        var ampmString = am ? 'am' : 'pm';
-
-        return {
-            hour: hour,
-            mins: out_minutes,
-            am: am
+    function formatTime(date, startTime, endTime) {
+        var toIsoTime = function (date, time) {
+            if (!date || !time) {
+                console.error('Invalid datetime: ', date, time);
+                return '';
+            }
+            return new Date(date + ' ' + time).toISOString();
         };
+
+        return [
+            toIsoTime(date, startTime),
+            toIsoTime(date, endTime)
+        ].join('/');
+    }
+    
+    function yyyymmdd(date) {
+        date = date || new Date();
+        var mm = date.getMonth() + 1;
+        var dd = date.getDate();
+
+        return [
+            date.getFullYear(),
+            mm<10 ? '0'+mm : mm,
+            dd<10 ? '0'+dd : dd
+        ].join('-');
     }
 
     // Load JSON data to dropdown template 
