5b9b3dd90de028b2a530a3952ab2dfedf983ba45
langdonms <langdon.sanders@gmail.com>
2016-10-23 12:23:02 -0400

    Custom Icons for Rider/Driver
    Changed way of creating the layers from plain L.geoJSON to calling
    pointToLayer, this way we can assign custom markers as defined by the
    driverIcon and riderIcon variables.

--- carpool-map/js/map.js
+++ carpool-map/js/map.js
@@ -15,16 +15,41 @@
         retainZoomLevel: false,
     }).addTo(map);
 
+//custom icons
+    var riderIcon = new L.icon({
+        iconUrl: '/carpool-map/img/rider_red.png',
+        iconSize:     [30, 30], // size of the icon
+        iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+    
+    var driverIcon = new L.icon({
+        iconUrl: '/carpool-map/img/maki-car-15-blue.png',
+        iconSize:     [30, 30], // size of the icon
+        iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+
 //data location
     var jsonRider = [{"count":"1","zip":"20111","state":"VA","latitude":" 38.769697","longitude":" -77.44915","city":"Manassas","full_state":"Virginia","latitude_numeric":38.7697,"longitude_numeric":-77.4492,"latlong":{"x":38.7696952819824,"y":-77.4491500854492}}];
     var jsonDriver = [{"count":"1","zip":"20111","state":"VA","latitude":" 38.769697","longitude":" -77.44915","city":"Manassas","full_state":"Virginia","latitude_numeric":38.7697,"longitude_numeric":-77.4492,"latlong":{"x":38.7696952819824,"y":-77.4491500854492}}];
 
-// create geoJSON by parsing JSON with geojson.js library
+// create geoJSON layer by parsing JSON with geojson.js library
     var jsonRiderParse = GeoJSON.parse(jsonRider, {Point: ["latitude_numeric", "longitude_numeric"]});
     var jsonDriverParse = GeoJSON.parse(jsonDriver, {Point: ["latitude_numeric", "longitude_numeric"]});
     
-    var jsonRiderLayer = L.geoJSON(jsonRiderParse).addTo(map);
-    var jsonDriverLayer = L.geoJSON(jsonDriverParse).addTo(map);
+//lot the geoJSON feaures with markesr so we can use custom icons
+    var jsonRiderLayer = L.geoJSON(jsonRiderParse, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {icon: riderIcon});
+        }
+    }).addTo(map);
+    
+    var jsonDriverLayer = L.geoJSON(jsonDriverParse, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {icon: driverIcon});
+        }
+    }).addTo(map);
     
 // load these layers into a group for the layer control
     var overlayGroup = L.layerGroup([jsonRiderLayer, jsonDriverLayer]);
