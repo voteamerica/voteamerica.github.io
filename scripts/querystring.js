feedbd2f9b132362a509655eb49650ee9b8960a1
Richard Westenra <rjwestenra@gmail.com>
2016-10-23 01:06:35 +0100

    Add Query String utility library

---
+++ scripts/querystring.js
@@ -0,0 +1,40 @@
+var QueryString = {
+  regex: function(name) {
+    return new RegExp('[\\?&](' + name + ')=?([^&#]*)');
+  },
+
+  get: function(name, query) {
+    query = query || window.location.search;
+    var match = query.match( this.regex(name) );
+    if (!match) {
+      return false;
+    } else if (match[2]) {
+      return decodeURIComponent(match[2]);
+    } else {
+      return true;
+    }
+  },
+
+  set: function(name, value, query) {
+    query = query || window.location.search;
+    var regex = this.regex(name),
+      match = regex.exec(query),
+      pair = name + '=' + encodeURIComponent(value);
+
+    if (!query.length) {
+      // If there are no existing queries then create new one:
+      return '?' + pair;
+    } else if (match) {
+      // If there is an existing query for this name then update the value:
+      return query.replace(regex, match[0].charAt(0) + pair);
+    } else {
+      // If there are existing queries but not for this name then add it to the end:
+      return query + '&' + pair;
+    }
+  },
+
+  remove: function(name, query) {
+    query = query || window.location.search;
+    return query.replace(this.regex(name), '');
+  }
+};
