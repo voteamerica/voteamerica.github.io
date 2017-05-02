3aab8d48d1fc8a80672e3d35942d73488972e6c8
jkbits1 <jkbits1@users.noreply.github.com>
2017-05-02 01:58:05 +0100

    display relevant info for request success or failure

---
+++ scripts/thanks.js
@@ -0,0 +1,33 @@
+if (!remoteUrl) {
+  var remoteUrl = "https://api.carpoolvote.com/live";
+}
+
+const handleBackendResponse = () => {
+  var data = tinyQuery.getAll();
+
+  var heading             = document.getElementById("thanks-header");
+  var introPara           = document.getElementById("introPara");
+  var successInfo         = document.getElementById("requestSuccessInfo");
+  var responseDetails     = document.getElementById("responseDetails");
+  var responseCode        = document.getElementById("responseCode");
+  var responseText        = document.getElementById("responseText");
+
+  var headingFailureText  = document.getElementById("sorryHeading").innerHTML;
+  var paraFailureText     = document.getElementById("sorryPara").innerHTML;
+
+  if (data.code != "0") {
+    heading.innerHTML   = headingFailureText;
+    introPara.innerHTML = paraFailureText;
+
+    successInfo.setAttribute("hidden", "hidden");
+    successInfo.setAttribute("style", "display:none");    
+
+    responseDetails.removeAttribute("hidden");
+    responseDetails.setAttribute("style", "");    
+
+    responseCode.innerHTML = data.code;
+    responseText.innerHTML = data.info;
+  }
+} 
+
+window.onload = handleBackendResponse; 
