2b8c13bba12252e2d58321d466e2c5f10f1d3cb8
jkbits1 <jkbits1@users.noreply.github.com>
2017-05-06 20:07:13 +0100

    tidying

--- scripts/thanks.js
+++ scripts/thanks.js
@@ -1,33 +1,25 @@
+var data = tinyQuery.getAll();
 
-// var data = tinyQuery.getAll();
+var heading             = document.getElementById("thanks-header");
+var introPara           = document.getElementById("introPara");
+var successInfo         = document.getElementById("requestSuccessInfo");
+var responseDetails     = document.getElementById("responseDetails");
+var responseCode        = document.getElementById("responseCode");
+var responseText        = document.getElementById("responseText");
 
+var headingFailureText  = document.getElementById("sorryHeading").innerHTML;
+var paraFailureText     = document.getElementById("sorryPara").innerHTML;
 
-// const handleBackendResponse = () => {
-  var data = tinyQuery.getAll();
+if (data.code != "0") {
+  heading.innerHTML   = headingFailureText;
+  introPara.innerHTML = paraFailureText;
 
-  var heading             = document.getElementById("thanks-header");
-  var introPara           = document.getElementById("introPara");
-  var successInfo         = document.getElementById("requestSuccessInfo");
-  var responseDetails     = document.getElementById("responseDetails");
-  var responseCode        = document.getElementById("responseCode");
-  var responseText        = document.getElementById("responseText");
+  successInfo.setAttribute("hidden", "hidden");
+  successInfo.setAttribute("style", "display:none");    
 
-  var headingFailureText  = document.getElementById("sorryHeading").innerHTML;
-  var paraFailureText     = document.getElementById("sorryPara").innerHTML;
+  responseDetails.removeAttribute("hidden");
+  responseDetails.setAttribute("style", "");    
 
-  if (data.code != "0") {
-    heading.innerHTML   = headingFailureText;
-    introPara.innerHTML = paraFailureText;
-
-    successInfo.setAttribute("hidden", "hidden");
-    successInfo.setAttribute("style", "display:none");    
-
-    responseDetails.removeAttribute("hidden");
-    responseDetails.setAttribute("style", "");    
-
-    responseCode.innerHTML = data.code;
-    responseText.innerHTML = data.info;
-  }
-// } 
-
-// window.onload = handleBackendResponse; 
+  responseCode.innerHTML = data.code;
+  responseText.innerHTML = data.info;
+}
