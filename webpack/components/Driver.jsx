b4798f33ea3396bbc4302e39a14ab57fdd4e0a01
jkbits1 <jkbits1@users.noreply.github.com>
2018-08-15 03:01:35 +0100

    added empty react driver component

---
+++ webpack/components/Driver.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Driver extends Component {
+    render() {
+        return (
+            <div>Driver Info xx</div>
+        )
+    }
+}
+
+export default Driver;
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Driver from './components/Driver.jsx';
+
 console.log("entry.js sttxx xnloaded");
+
+class App extends Component {
+    render() {
+        return <Driver />
+    }
+}
+
+render(<App />, document.getElementById('root'));
