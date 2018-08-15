32f730e2db0a5310e4bbcf26db7e36dd6a10989a
jkbits1 <jkbits1@users.noreply.github.com>
2018-08-15 02:44:22 +0100

    support webpack for client

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const outputPath = path.resolve(__dirname, "scripts");
+
+console.log("outputPath", outputPath);
+
+module.exports = {
+    watchOptions: {
+        poll: 1000
+    },
+    entry: "./webpack/entry.js",
+    mode: "development",
+    output: {
+        path: outputPath,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: [/(node_modules)/],
+                use: [
+                    {
+                        loader: "babel-loader",
+                        query: {
+                            presets: ["react"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
