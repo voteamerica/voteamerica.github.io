93178f0e5155db8bdd338e093eb6b32a041ae7e4
jkbits1 <jkbits1@users.noreply.github.com>
2018-08-16 22:38:33 +0100

    add react username/password fields, apply prettier formatting

--- webpack/components/App.jsx
+++ webpack/components/App.jsx
@@ -6,10 +6,10 @@ import Driver from './Driver.jsx';
 import { login, loginSuccess } from '../actions/index.js';
 
 const mapStateToProps = state => {
-    const { apiInfo, loginInfo } = state;
+  const { apiInfo, loginInfo } = state;
 
-    return { apiInfo, loginInfo };
-}
+  return { apiInfo, loginInfo };
+};
 
 // const mapDispatchToProps = dispatch => {
 //     return {
@@ -18,43 +18,78 @@ const mapStateToProps = state => {
 // }
 
 const mapDispatchToProps = {
-    login,
-    loginSuccess
+  login,
+  loginSuccess
 };
 
 class AppBase extends Component {
+  handleLoginRequestClick(self) {
+    return event => {
+      const { apiInfo, login } = self.props;
 
-    handleLoginRequestClick(self) {
-        return event => {
-            const { apiInfo, login } = self.props;
+      const token = event.target.value;
 
-            const token = event.target.value;
+      return login(apiInfo.apiUrl, 'a', 'b');
+    };
+  }
 
-            return login(apiInfo.apiUrl, 'a', 'b');
-        }
-    }
+  handleLoginSuccessClick(self) {
+    return event => {
+      const { loginSuccess } = self.props;
 
-    handleLoginSuccessClick(self) {
-        return event => {
-            const { loginSuccess } = self.props;
+      const token = event.target.value;
 
-            const token = event.target.value;
+      return loginSuccess(token);
+    };
+  }
 
-            return loginSuccess(token);
-        }
-    }
+  handleUsernameChange(self) {
+    return event => {
+      console.log('username', event.target.value);
+    };
+  }
 
-    render() {    
-        return (
-            <div>
-                <div>
-                    <button onClick={this.handleLoginRequestClick(this)}>Login</button>
-                </div>
-                <Driver />
-            </div>)
-    }
+  render() {
+    return (
+      <div>
+        <div id="enter-uuid" className="form-group">
+          <label htmlFor="username">Please enter your username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="form-input form-input--medium"
+            placeholder="e.g. 1a2b-3c4d-5e6f-7g8hi9j0klmno"
+            required
+            onChange={this.handleUsernameChange(this)}
+          />
+          <div className="help-block with-errors" />
+        </div>
+        <div id="enter-phone" className="form-group">
+          <label htmlFor="password">Enter your password</label>
+          <input
+            type="tel"
+            id="password"
+            name="password"
+            className="form-input form-input--medium"
+            placeholder="e.g. 1234567890"
+            required
+          />
+          <div className="help-block with-errors" />
+        </div>
+
+        <div>
+          <button onClick={this.handleLoginRequestClick(this)}>Login</button>
+        </div>
+        <Driver />
+      </div>
+    );
+  }
 }
 
-const App = connect(mapStateToProps, mapDispatchToProps)(AppBase);
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppBase);
 
 export default App;
