1c73c6e5c811ffeb9465f0b153bf8bb63b49b129
jkbits1 <jkbits1@users.noreply.github.com>
2018-09-07 02:24:32 +0100

    get rider details from actual props

--- webpack/components/RidersPlus.jsx
+++ webpack/components/RidersPlus.jsx
@@ -6,11 +6,11 @@ const mapStateToProps = state => {
   // const { apiInfo, loginInfo, driversInfo } = state;
   const { driversInfo } = state;
   // const riders = 'testr';
-  // const riders = driversInfo.drivers;
-  const riders = [
-    { DriverFirstName: 't1', DriverEmail: 'x@test.com', DriverLastName: 'l1' },
-    { DriverFirstName: 't2', DriverEmail: 'y@test.com', DriverLastName: 'l2' }
-  ];
+  const riders = driversInfo.drivers;
+  // const riders = [
+  //   { DriverFirstName: 't1', DriverEmail: 'x@test.com', DriverLastName: 'l1' },
+  //   { DriverFirstName: 't2', DriverEmail: 'y@test.com', DriverLastName: 'l2' }
+  // ];
 
   return { riders };
 };
