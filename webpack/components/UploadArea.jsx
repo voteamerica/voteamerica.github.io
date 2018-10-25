b755abc1bcff9ca93ff28da29c8548e71ace0472
jkbits1 <jkbits1@users.noreply.github.com>
2018-10-25 21:26:50 +0100

    display response after upload

--- webpack/components/UploadArea.jsx
+++ webpack/components/UploadArea.jsx
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { postUploadFile, uploadFileChosen } from '../actions/index.js';
+
+const SubmitButton = styled.button`
+  margin-top: 0px;
+  margin-bottom: 20px;
+`;
+
+const UploadInfoDiv = styled.div`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+const UploadInfoListItemDiv = styled.div`
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
 
 const mapStateToProps = state => {
   const { apiInfo, loginInfo, uploadInfo } = state;
@@ -54,7 +70,15 @@ class UploadAreaBase extends Component {
   }
 
   render() {
-    const { loginInfo } = this.props;
+    const { uploadInfo, loginInfo } = this.props;
+
+    const fileChoiceStyle = { marginBottom: 20 };
+
+    const notAllRowsInputIntoDb =
+      uploadInfo.processingResults.recordsReceived >
+      uploadInfo.processingResults.uploadCount;
+
+    const errorsLoadedStyle = notAllRowsInputIntoDb ? { color: 'red' } : {};
 
     return (
       <div id="uploadArea">
@@ -62,7 +86,10 @@ class UploadAreaBase extends Component {
           <div>
             <h2 style={{ marginTop: 40 }}>Upload Area</h2>
             <p>
-              NOTE: take care not to upload the same file twice, as this will
+              <span>
+                <strong>IMPORTANT:</strong>
+              </span>
+              &nbsp; take care not to upload the same file twice, as this will
               create duplicates on the system
             </p>
             <form
@@ -71,24 +98,101 @@ class UploadAreaBase extends Component {
               encType="multipart/form-data"
             >
               <div>
-                <div>
+                <div style={fileChoiceStyle}>
                   <label htmlFor="file">Choose file to upload</label>
                   <input
+                    style={{ marginTop: 10 }}
                     type="file"
                     id="file"
                     name="file"
                     onChange={this.fileChoiceButtonClickHandler(this)}
                   />
                 </div>
-                <div>
-                  <button
-                    className="button button--large"
-                    id="postUploadButton"
-                    onClick={this.postButtonClickHandler(this)}
-                  >
-                    Submit
-                  </button>
-                </div>
+                {uploadInfo.fileChosen === true &&
+                uploadInfo.fileBeingProcessed === false ? (
+                  <div>
+                    <SubmitButton
+                      className="button button--large"
+                      id="postUploadButton"
+                      onClick={this.postButtonClickHandler(this)}
+                    >
+                      Submit
+                    </SubmitButton>
+                  </div>
+                ) : (
+                  <div>
+                    {uploadInfo.fileChosen === true &&
+                    uploadInfo.fileBeingProcessed === true ? (
+                      <div>File being processed </div>
+                    ) : (
+                      false
+                    )}
+                  </div>
+                )}
+                {uploadInfo.fileChosen === true &&
+                uploadInfo.fileBeingProcessed === false &&
+                uploadInfo.showProcessingResults === true ? (
+                  <div>
+                    {notAllRowsInputIntoDb ? (
+                      <div>
+                        <span>
+                          <strong style={{ color: 'red' }}>IMPORTANT:</strong>{' '}
+                          Not all records were input into the db. Review the
+                          information below to resolve this.
+                        </span>
+                      </div>
+                    ) : (
+                      false
+                    )}
+
+                    <UploadInfoDiv>
+                      Records received:&nbsp;
+                      {uploadInfo.processingResults.recordsReceived}
+                    </UploadInfoDiv>
+                    <UploadInfoDiv>
+                      Records loaded into db:&nbsp;
+                      <span style={errorsLoadedStyle}>
+                        {uploadInfo.processingResults.uploadCount}
+                      </span>
+                    </UploadInfoDiv>
+                    {uploadInfo.processingResults.inputErrorsCount &&
+                    uploadInfo.processingResults.inputErrorsCount > 0 ? (
+                      <UploadInfoDiv>
+                        Input errors count:&nbsp;
+                        {uploadInfo.processingResults.inputErrorsCount}
+                      </UploadInfoDiv>
+                    ) : (
+                      false
+                    )}
+                    {uploadInfo.processingResults.inputErrors &&
+                    uploadInfo.processingResults.inputErrors.length > 0 ? (
+                      <UploadInfoDiv>
+                        Input errors:&nbsp;
+                        <ul>
+                          {uploadInfo.processingResults.inputErrors.map(
+                            (r, index) => (
+                              <li key={index}>
+                                <UploadInfoListItemDiv>
+                                  Error type: {r.type.toString()}
+                                </UploadInfoListItemDiv>
+                                <UploadInfoListItemDiv>
+                                  Db error: {r.error.toString()}
+                                </UploadInfoListItemDiv>
+                                <UploadInfoListItemDiv>
+                                  Data row: {JSON.stringify(r.data)}
+                                </UploadInfoListItemDiv>
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </UploadInfoDiv>
+                    ) : (
+                      false
+                    )}
+                  </div>
+                ) : (
+                  false
+                )}
               </div>
             </form>
           </div>
@@ -101,7 +205,10 @@ class UploadAreaBase extends Component {
 }
 
 /*
-
+{uploadInfo.fileChosen === true &&
+                      uploadInfo.fileBeingProcessed === true ? <div>File being processed </div> :
+                  false
+                )}
 */
 
 const UploadArea = connect(
--- webpack/reducers/uploadInfo.js
+++ webpack/reducers/uploadInfo.js
@@ -1,6 +1,24 @@
-import { UPLOAD_FILE_CHOSEN } from '../actions/types.js';
+import {
+  UPLOAD_FILE_CHOSEN,
+  POST_UPLOAD,
+  postUploadAsyncTypes
+} from '../actions/types.js';
 
-const uploadInfo = (state = { fileDetails: {}, fileChosen: false }, action) => {
+const uploadInfo = (
+  state = {
+    fileDetails: {},
+    fileChosen: false,
+    fileBeingProcessed: false,
+    showProcessingResults: false,
+    processingResults: {
+      recordsReceived: 0,
+      uploadCount: 0,
+      inputErrors: [],
+      inputErrorsCount: 0
+    }
+  },
+  action
+) => {
   switch (action.type) {
     case UPLOAD_FILE_CHOSEN: {
       const { payload } = action;
@@ -13,10 +31,66 @@ const uploadInfo = (state = { fileDetails: {}, fileChosen: false }, action) => {
       const newState = {
         ...state,
         fileDetails,
-        fileChosen: true
+        fileChosen: true,
+        showProcessingResults: false
       };
 
       return newState;
+    }
+
+    case POST_UPLOAD: {
+      const newState = {
+        ...state,
+        fileBeingProcessed: true,
+        showProcessingResults: false
+      };
+
+      return newState;
+    }
+
+    case postUploadAsyncTypes.success: {
+      const { data: processingResults } = action.payload;
+      const showProcessingResults = true;
+      const fileBeingProcessed = false;
+
+      if (processingResults.recordsReceived !== undefined)
+        return {
+          ...state,
+          showProcessingResults,
+          fileBeingProcessed,
+          processingResults
+        };
+      else if (
+        processingResults.recordsReceived === undefined &&
+        processingResults.err &&
+        processingResults.err.inputErrors
+      ) {
+        // system processed csv successfully but at least some rows were not added to db
+
+        let newProcessingResults = {
+          inputErrors: processingResults.err.inputErrors,
+          inputErrorsCount: processingResults.err.inputErrorsCount
+        };
+
+        const replyDetailsLength = processingResults.err.replyDetailsLength;
+
+        if (replyDetailsLength) {
+          newProcessingResults = {
+            ...newProcessingResults,
+            recordsReceived: replyDetailsLength.recordsReceived,
+            uploadCount: replyDetailsLength.uploadCount
+          };
+        }
+
+        return {
+          ...state,
+          showProcessingResults,
+          fileBeingProcessed,
+          processingResults: newProcessingResults
+        };
+      } else {
+        return state;
+      }
     }
 
     default:
